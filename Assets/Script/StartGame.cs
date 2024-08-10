44690c25a099cdbcf8f3c1d57ef5824872dc7130
SrpNumfah <jellyboofah2000@gmail.com>
2024-08-10 18:05:42 +0700

    add start scene

--- Assets/Script/StartGame.cs
+++ Assets/Script/StartGame.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField, TabGroup("Button")] private Button startGame;
+
+    private void Awake()
     {
-        
+        startGame.onClick.AddListener(() => ChangeScene());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ChangeScene()
     {
-        
+        SceneManager.LoadScene(1);
     }
 }
